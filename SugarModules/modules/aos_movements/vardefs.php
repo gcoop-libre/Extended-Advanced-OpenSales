a8e50ac9c2d6764a78870f4a0fbe0b9b98ed36bd
Jose <jose@gcoop.coop>
2011-12-21 17:58:34 -0300

    I changed the type of the fields:
    AOS_Products:
    
        - stock
        - order_point
    
    aos_movements
    
        - quantity

--- SugarModules/modules/aos_movements/vardefs.php
+++ SugarModules/modules/aos_movements/vardefs.php
@@ -4,7 +4,7 @@
  * @package Advanced OpenSales for SugarCRM
  * @subpackage Movements
  * @copyright José C. Massón
- * 
+ *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU AFFERO GENERAL PUBLIC LICENSE as published by
  * the Free Software Foundation; either version 3 of the License, or
@@ -23,55 +23,55 @@
  * @author José C. Massón <jose@gcoop.coop>
  */
 $dictionary['aos_movements'] = array(
-	'table'=>'aos_movements',
-	'audited'=>true,
-	'fields'=>array (
-  'quantity' => 
-  array (
-    'required' => true,
-    'name' => 'quantity',
-    'vname' => 'LBL_QUANTITY',
-    'type' => 'int',
-    'massupdate' => 0,
-    'comments' => '',
-    'help' => '',
-    'importable' => 'true',
-    'duplicate_merge' => 'disabled',
-    'duplicate_merge_dom_value' => '0',
-    'audited' => true,
-    'reportable' => true,
-    'len' => '255',
-    'size' => '20',
-    'disable_num_format' => '',
-  ),
-  'direction' => 
-  array (
-    'required' => true,
-    'name' => 'direction',
-    'vname' => 'LBL_DIRECTION',
-    'type' => 'radioenum',
-    'massupdate' => 0,
-    'default' => 'in',
-    'comments' => '',
-    'help' => '',
-    'importable' => 'true',
-    'duplicate_merge' => 'disabled',
-    'duplicate_merge_dom_value' => '0',
-    'audited' => true,
-    'reportable' => true,
-    'len' => 100,
-    'size' => '20',
-    'options' => 'utn_direction_list',
-    'studio' => 'visible',
-    'dbType' => 'enum',
-    'separator' => '<br>',
-  ),
-),
-    'relationships'=>array ( 
-    ),
-	'optimistic_locking'=>true,
-);
-if (!class_exists('VardefManager')){
-        require_once('include/SugarObjects/VardefManager.php');
+                                   'table'=>'aos_movements',
+                                   'audited'=>true,
+                                   'fields'=>array (
+                                       'quantity' =>
+                                       array (
+                                               'required' => true,
+                                               'name' => 'quantity',
+                                               'vname' => 'LBL_QUANTITY',
+                                               'type' => 'float',
+                                               'massupdate' => 1,
+                                               'default' => '0.000',
+                                               'comments' => '',
+                                               'help' => '',
+                                               'importable' => true,
+                                               'duplicate_merge' => 'disabled',
+                                               'duplicate_merge_dom_value' => '0',
+                                               'audited' => true,
+                                               'len' => '18',
+                                               'precision' => '3',
+                                       ),
+                                       'direction' =>
+                                       array (
+                                               'required' => true,
+                                               'name' => 'direction',
+                                               'vname' => 'LBL_DIRECTION',
+                                               'type' => 'radioenum',
+                                               'massupdate' => 0,
+                                               'default' => 'in',
+                                               'comments' => '',
+                                               'help' => '',
+                                               'importable' => 'true',
+                                               'duplicate_merge' => 'disabled',
+                                               'duplicate_merge_dom_value' => '0',
+                                               'audited' => true,
+                                               'reportable' => true,
+                                               'len' => 100,
+                                               'size' => '20',
+                                               'options' => 'utn_direction_list',
+                                               'studio' => 'visible',
+                                               'dbType' => 'enum',
+                                               'separator' => '<br>',
+                                       ),
+                                   ),
+                                   'relationships'=>array (
+                                   ),
+                                   'optimistic_locking'=>true,
+                               );
+if (!class_exists('VardefManager'))
+{
+    require_once('include/SugarObjects/VardefManager.php');
 }
 VardefManager::createVardef('aos_movements','aos_movements', array('basic','assignable'));
